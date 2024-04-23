b8b096a0f0858033411bf7846bda02d4476d9f98
jacksonjjones <jacksonjjones1@gmail.com>
2024-04-23 11:21:40 -0500

    added thought model

---
+++ models/Thought.js
@@ -0,0 +1,35 @@
+const { Schema, model } = require("mongoose");
+
+const thoughtSchema = new Schema(
+  {
+    thoughtText: {
+      type: String,
+      required: true,
+      minLenght: 1,
+      maxlength: 280,
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now,
+      get: (timestamp) => new Date(timestamp).toLocaleString(),
+    },
+    username: {
+      type: String,
+      required: true,
+    },
+  },
+  {
+    toJSON: {
+      getters: true,
+    },
+    id: false,
+  }
+);
+
+thoughtSchema.virtual("reactionCount").get(function () {
+  return this.reactions.length;
+});
+
+const Thought = model("Thought", thoughtSchema);
+
+module.exports = Thought;
